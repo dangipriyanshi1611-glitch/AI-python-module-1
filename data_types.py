e0b3b9984326070c3c047c9650e59cd087cd0682
dangipriyanshi1611-glitch <dangipiyu1611@gmail.com>
2026-08-12 17:33:50 +0530

    Create data_types.py

---
+++ data_types.py
@@ -0,0 +1,9 @@
+name="Priya"              #string
+age=19                    #integer
+percentage=85.5            #float
+is_student=True          #boolean
+
+print(type(name))
+print(type(age))
+print(type(percentage))
+print(type(is_student))
